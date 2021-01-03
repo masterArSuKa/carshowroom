357319bce207b29b35c5b05b6dba3a0e7a8ee81a
Arnauv Kaushik <x19231661@student.ncirl.ie>
2021-01-03 03:50:08 +0000

    car3

--- dbconnection.php
+++ dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$db=mysqli_connect("localhost","root","","car_showroom");
+$db=mysqli_connect("car.cqn8jr4zs6zp.us-east-1.rds.amazonaws.com","admin","12345678","car_showroom");
 if(!$db)
 {
 	die("could not connect".mysqli_error());
